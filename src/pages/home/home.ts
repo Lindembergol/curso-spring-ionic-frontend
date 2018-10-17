f2dc9236b25ce9e8c63be349a3e23b0b01e324ef
Lindemberg Oliveira <linndemberg@outlook.com>
2018-10-17 20:48:04 -0300

    Lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+@IonicPage() //Fala pra aplicação que a classe HomePage é uma pagina e que podera ser referenciada entre aspas, na forma de String. 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html' //Define qual arquivo Html esta sendo controlado
 })
 export class HomePage {
 
